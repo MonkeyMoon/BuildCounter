2f0d6b48d133d374cb52f9e19ac9645484564171
Laurent Victorino <lvictorino@monkeymoon.net>
2020-01-21 10:30:20 +0100

    :card_file_box: Adding Unity scriptable object and asset files.

---
+++ UnityFiles/BuildVersionData.cs
@@ -0,0 +1,28 @@
+﻿// Written by @valryon for MONKEYMOON
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MM.Utils
+{
+    [CreateAssetMenu(fileName = "BuildVersion", menuName = "Build Version")]
+    public class BuildVersionData : ScriptableObject
+    {
+        public int major;
+        public int minor;
+        public int patch;
+        public string commit_hash;
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}.{2}", major, minor, patch);
+        }
+
+        public string ToStringWithCommit()
+        {
+            return ToString() + " " + commit_hash;
+        }
+    }
+}
+
